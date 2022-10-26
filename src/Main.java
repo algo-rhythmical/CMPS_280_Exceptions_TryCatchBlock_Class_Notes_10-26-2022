24addaba50c5f0ad23c8d5613b3cf879ec7e2c48
Derrick Poon <w0748346@ad.southeastern.edu>
2022-10-26 15:11:25 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+/*Today's agenda:
+
+-Exceptions:
+    - represent specific types of errors/edge cases in your code.
+    - They all inherit from the "Throwable" class.
+    - most of the time, we will use a child class of throwable, called exception, as a parent class
+    - whenever an exception happens, your program crashes.
+    - 'throws': used in method/constructor declarations to tell Java that said method/constructor
+        may or may NOT throw an exception.
+    - 'throw': inside of a method/constructor, to actually use an exception.
+
+-Try/catch block:
+    - used to deal with exceptions that may happen.
+    - syntax:
+
+        try {
+            //run the code that may or may not throw an exception
+        }
+        catch(Exception exceptionObject) {     //type your trying to catch
+            //where you deal with the exception that was thrown
+        }
+
+ */
+
+
+
+public class Main {
+}
